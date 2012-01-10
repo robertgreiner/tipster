a77b5e63d1da2436fc4aa5931e3bf54469ab36c5
Robert Greiner <robert@robertgreiner.com>
2012-01-10 15:44:33 -0600

    made spec more readable

--- spec/commands/git/latest_commit_spec.rb
+++ spec/commands/git/latest_commit_spec.rb
@@ -2,9 +2,8 @@ require 'helper'
 require 'commands/git/latest_commit'
 
 describe "The latest git commit" do
-  it "should return the most recent hash" do
-    result = LatestCommit.new.id
-    true.should == true
-    result.empty?.should == false
+  it "should return the most recent SHA" do
+    sha = LatestCommit.new.id
+    sha.length.should be 40
   end
 end
