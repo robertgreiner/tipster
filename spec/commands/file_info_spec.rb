4fc94295d5a9f577e42219591ee0b3b325f0c9d3
Robert Greiner <robert@robertgreiner.com>
2012-01-10 20:51:53 -0600

    standardizing tests based on working directory.

--- spec/commands/file_info_spec.rb
+++ spec/commands/file_info_spec.rb
@@ -3,11 +3,11 @@ require 'commands/file_info'
 
 describe "Line count" do
   it "should return the number of lines in a file" do
-    file_name = "../spec/stub/fake_file.txt"
+    file_name = "./stub/fake_file.txt"
     FileInfo.new(file_name).line_count.should be 10
   end
   it "should raise an error if the file does not exist" do
-    file_name = "../spec/stub/does-not-exist.txt"
+    file_name = "./stub/does-not-exist.txt"
     expect { FileInfo.new(file_name).line_count }.to raise_error(Errno::ENOENT)
   end
 end
